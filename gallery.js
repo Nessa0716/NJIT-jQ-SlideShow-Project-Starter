c1d38f2c1516062248bcd5fd513d92e8b6793dbf
Nessa0716 <vonessa0716@gmail.com>
2024-11-15 12:53:35 -0500

    doen with nava dn timer!!!!

--- gallery.js
+++ gallery.js
@@ -1,36 +1,39 @@
-let mCurrentIndex = 0
-let mImages = [] 
-const mUrl ='images.json' 
-const mWaitTime = 5000 
+let mCurrentIndex = 0 // Tracks the current image index
+let mImages = [] // Array to hold GalleryImage objects
+const mUrl = 'images.json' // Replace with actual JSON URL
+const mWaitTime = 10000 // Timer interval in milliseconds
 
 $(document).ready(() => {
-  $('.details').hide() 
+  $('.details').slideToggle() // Hide details initially
 
   // Call a function here to start the timer for the slideshow
-
+  startTimer()
   // Select the moreIndicator button and add a click event to:
   // - toggle the rotation classes (rot90 and rot270)
   // - slideToggle the visibility of the .details section
-$('.moreIndicator').on('click', function () {
-  $('.moreIndicator').toggleClass('rot270 rot90')
-  $('.details').slideToggle()
-})
+  $('.moreIndicator').on('click', function () {
+    $(this).toggleClass('rot90').toggleClass('rot270');
+    $('.details').slideToggle(); // Toggle visibility of .details on click
+  });
   // Select the "Next Photo" button and add a click event to call showNextPhoto
-
+  $('#nextPhoto').on('click', function () {
+    showNextPhoto()
+  })
   // Select the "Previous Photo" button and add a click event to call showPrevPhoto
-
+  $('#prevPhoto').on('click', function () {
+    showPrevPhoto()
+  })
   // Call fetchJSON() to load the initial set of images
-  fetchJSON() 
+  fetchJSON()
 })
 
 // Function to fetch JSON data and store it in mImages
-function fetchJSON () {
+function fetchJSON() {
   // Use $.ajax here to request the JSON data from mUrl
   $.ajax({
-    type: "GET",
+    type: 'GET',
     url: mUrl,
     success: function (data) {
-      //change photo
       mImages = data.images
       swapPhoto()
     }
@@ -40,29 +43,67 @@ function fetchJSON () {
 }
 
 // Function to swap and display the next photo in the slideshow
-function swapPhoto () {
+function swapPhoto() {
   // Access mImages[mCurrentIndex] to update the image source and details
   // Update the #photo element's src attribute with the current image's path
-  // Update the .Name, .description, and .date elements with the current image's details
+  // Update the .location, .description, and .date elements with the current image's details
+  startTimer()
   let imgInfo = mImages[mCurrentIndex]
-  $('#photo').attr('src', imgInfo.imgPath)
+  $('#photo, .name, .description, .date').fadeOut(510);
+  setTimeout(() => {
+    $('#photo').attr('src', imgInfo.imgPath)
+    $('.date').text(`Date: ${imgInfo.ship}`)
+    $('.name').text(`Name: ${imgInfo.pilot}`)
+    $('.description').text(`Description: ${imgInfo.description}`)
+    $('#photo, .name, .description, .date').fadeIn(500);
+  }, 500);
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
-function showNextPhoto () {
-  // Increment mCurrentIndex and call swapPhoto()
+function showNextPhoto() {
+  mCurrentIndex++;
+  if (mCurrentIndex > mImages.length - 1) {
+    mCurrentIndex = 0
+  }
+  swapPhoto()
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
-function showPrevPhoto () {
+function showPrevPhoto() {
   // Decrement mCurrentIndex and call swapPhoto()
+  mCurrentIndex--;
+  if (mCurrentIndex < 0) {
+    mCurrentIndex = mImages.length - 1
+  }
+  swapPhoto()
   // Ensure it loops to the end if mCurrentIndex is less than 0
 }
 
+let timerInterval;
+let countdownInterval
 // Starter code for the timer function
-function startTimer () {
+function startTimer() {
   // Create a timer to automatically call `showNextPhoto()` every mWaitTime milliseconds
   // Consider using setInterval to achieve this functionality
   // Hint: Make sure only one timer runs at a time
+  if (timerInterval) {
+    clearInterval(timerInterval)
+  }
+  timerInterval = setInterval(() => showNextPhoto(), mWaitTime)
+  seconds = 11
+  if (countdownInterval) {
+    clearInterval(countdownInterval)
+  }
+  displayTimer()
+  countdownInterval = setInterval(() => displayTimer(), 1000)
+}
+
+let seconds = 11
+function displayTimer() {
+  seconds -= 1
+  if (seconds < 0) {
+    seconds = 11
+  }
+  $('#timer').text(`Time Before Automatically Moving to the Next: ${seconds}`)
 }
